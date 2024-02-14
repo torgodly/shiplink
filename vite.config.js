dcebd6c2272e04acefc9b1afd72712baacf1c0e2
Abdullah Alhaj <torgodly@gmail.com>
2024-02-15 00:09:15 +0200

    vita config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/user/theme.css', 'resources/css/filament/office/theme.css'],
             refresh: true,
         }),
     ],
