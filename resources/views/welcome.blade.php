d0779267c9ef97ebd92c51739fcd09c4979599c7
Abdullah Alhaj <torgodly@gmail.com>
2024-02-18 20:33:37 +0200

    panel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,29 @@
 <x-app-layout>
-    <div class="mt-8 flex sm:justify-end">
-        <div class="w-full max-w-2xl sm:text-end space-y-2">
-            <!-- Grid -->
-            <div class="grid grid-cols-2 sm:grid-cols-1 gap-3 sm:gap-2" >
-                <dl >
-                        <dt class="col-span- font-semibold text-gray-800 ">{{__('Subtotal:')}}</dt>
-                        <dd class="col-span- text-gray-500">1</dd>
-                </dl>
-
-
-            </div>
-            <!-- End Grid -->
+    <div class="flex justify-center items-center gap-3">
+        <div class="flex justify-center items-center h-screen">
+            <a href="user/login" class="text-blue-500 text-3xl font-bold">
+                <div class="border-2 rounded-xl border-blue-500 p-3 flex justify-center items-center">
+                    <x-tabler-user class="w-20 h-20 stroke-blue-500"/>
+                    Login to user panel
+                </div>
+            </a>
+        </div>
+        <div class="flex justify-center items-center h-screen">
+            <a href="office/login" class="text-blue-500 text-3xl font-bold">
+                <div class="border-2 rounded-xl border-blue-500 p-3 flex justify-center items-center">
+                    <x-tabler-building class="w-20 h-20 stroke-blue-500"/>
+                    Login to office panel
+                </div>
+            </a>
+        </div>
+        <div class="flex justify-center items-center h-screen">
+            <a href="admin/login" class="text-blue-500 text-3xl font-bold">
+                <div class="border-2 rounded-xl border-blue-500 p-3 flex justify-center items-center">
+                    <x-tabler-shield-check class="w-20 h-20 stroke-blue-500"/>
+                    Login to admin panel
+                </div>
+            </a>
         </div>
     </div>
+
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/admin/login');
+    return view('welcome');
 });
 
 Route::get('test', function () {
