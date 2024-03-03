442929843ceba4e55c48cba65d0f4158ca33bbb2
Abdullah Alhaj <torgodly@gmail.com>
2024-03-03 15:17:37 +0200

    fix qr code

--- app/Action/QRCodeAction.php
+++ app/Action/QRCodeAction.php
@@ -11,21 +11,7 @@ class QRCodeAction
     public static function QrCodeAction()
     {
         return Action::make('qr')
-            ->modalContent(fn(Model $record) => Qr::render($record->code, options: [
-            'size' => '300',
-            'margin' => '1',
-            'color' => 'rgba(27, 61, 212, 1)',
-            'back_color' => 'rgba(252, 252, 252, 1)',
-            'style' => 'round',
-            'hasGradient' => false,
-            'gradient_form' => 'rgb(69, 179, 157)',
-            'gradient_to' => 'rgb(241, 148, 138)',
-            'gradient_type' => 'vertical',
-            'hasEyeColor' => true,
-            'eye_color_inner' => 'rgb(255, 234, 41)',
-            'eye_color_outer' => 'rgb(255, 234, 41)',
-            'eye_style' => 'circle',
-        ], statePath: $record->code))
+            ->modalContent(fn(Model $record) => Qr::render($record->code, statePath: $record->code))
             ->icon('tabler-qrcode')
             ->label('QR Code')
             ->translateLabel()
